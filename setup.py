26cb92f3fbdef76cbb4ca7f209ccea0f536172a5
shahid265 <shahid265.sr@gmail.com>
2023-09-02 12:23:34 +0500

    update reamde.md

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='express_visualization_library',
     version='0.1.0',
@@ -9,4 +14,6 @@ setup(
     description='Express Visualization library is a Python package designed to streamline the data visualization process in a consultancy environment. This package aims to facilitate the quick transformation of data into insightful visualizations. It is developed on top of standard libraries such as Plotly and Circlify.',
     packages=find_packages(),    
     install_requires=['plotly', 'numpy', 'circlify', 'pytest'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
